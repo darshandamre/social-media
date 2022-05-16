b85a43da08c923c111dd27c0f418f9f8e4a35fef
Darshan Damre <darshandamre5@gmail.com>
2022-05-16 14:37:30 +0530

    fix server url

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -2,9 +2,12 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient("http://localhost:4000/graphql", {
-  credentials: "include"
-});
+export const client = new GraphQLClient(
+  process.env.GRAPHQL_ENDPOINT as string,
+  {
+    credentials: "include"
+  }
+);
 
 export const api = createApi({
   baseQuery: graphqlRequestBaseQuery({ client }),
