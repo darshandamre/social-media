a5e39f5ab29c0c757c88137c634ecb802004e3e9
Darshan Damre <darshandamre5@gmail.com>
2022-05-22 17:22:55 +0530

    removed unneccessary Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthPagesLayout, Login, Register, RequireAuth } from "./features/auth";
-import { Home, Layout } from "./features/common";
-import { ExploreFeed } from "./features/feed";
+import { Layout } from "./features/common";
+import { ExploreFeed, UserFeed } from "./features/feed";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<RequireAuth />}>
         <Route element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<UserFeed />} />
           <Route path="explore" element={<ExploreFeed />} />
         </Route>
       </Route>
