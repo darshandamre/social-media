03b15ea49bb8fe359d36934f0c428b91b21ffcab
Darshan Damre <darshandamre5@gmail.com>
2022-07-04 15:44:46 +0530

    remove rtkq endpoints

--- src/app/api/enhancedApi.ts
+++ src/app/api/enhancedApi.ts
@@ -1,44 +1,8 @@
 import { api as generatedApi } from "./baseApi";
 
 export const enhancedApi = generatedApi.enhanceEndpoints({
-  addTagTypes: ["User", "Post", "Likes", "Bookmarks", "UserFeed"],
+  addTagTypes: ["User", "Post", "UserFeed"],
   endpoints: {
-    // Me: {
-    //   providesTags: ["Me"]
-    // },
-    // Register: {
-    //   invalidatesTags: ["Me"]
-    // },
-    // Login: {
-    //   invalidatesTags: ["Me"]
-    // },
-    // Logout: {
-    //   invalidatesTags: ["Me"]
-    // },
-    // UserFeed: {
-    //   providesTags: ["UserFeed"]
-    // },
-    // Post: {
-    //   providesTags: result =>
-    //     result
-    //       ? [
-    //           { type: "Post", id: result.post?.id },
-    //           { type: "User", id: result.post?.authorId }
-    //         ]
-    //       : []
-    // },
-    // User: {
-    //   providesTags: result =>
-    //     result
-    //       ? [
-    //           ...(result.user?.posts?.map(({ id }) => ({
-    //             type: "Post" as const,
-    //             id
-    //           })) ?? ["Post" as const]),
-    //           { type: "User", id: result.user?.id }
-    //         ]
-    //       : ["User", "Post"]
-    // },
     Follow: {
       invalidatesTags: (result, _, { followId }) =>
         result?.follow ? [{ type: "User", id: followId }, "UserFeed"] : [],
@@ -159,30 +123,7 @@ export const enhancedApi = generatedApi.enhanceEndpoints({
         }
       }
     },
-    // LikedPosts: {
-    //   providesTags: result =>
-    //     result
-    //       ? [
-    //           ...(result.likedPosts?.map(post => ({
-    //             type: "Post" as const,
-    //             id: post.id
-    //           })) ?? []),
-    //           "Likes"
-    //         ]
-    //       : ["Post"]
-    // },
-    BookmarkedPosts: {
-      providesTags: result =>
-        result
-          ? [
-              ...(result.bookmarkedPosts?.map(post => ({
-                type: "Post" as const,
-                id: post.id
-              })) ?? []),
-              "Bookmarks"
-            ]
-          : ["Post"]
-    },
+
     EditUser: {
       onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
         try {
@@ -216,132 +157,6 @@ export const enhancedApi = generatedApi.enhanceEndpoints({
           );
         } catch (err) {
           console.error(err);
-        }
-      }
-    },
-    // Like: {
-    //   invalidatesTags: (result, _, { postId }) =>
-    //     result?.like ? [{ type: "Post", id: postId }, "Likes"] : [],
-    //   onQueryStarted: async ({ postId }, { dispatch, queryFulfilled }) => {
-    //     const userFeedPatchResult = dispatch(
-    //       enhancedApi.util.updateQueryData("UserFeed", undefined, draft => {
-    //         const idx = draft.userFeed.findIndex(post => post.id === postId);
-    //         if (idx > -1) {
-    //           draft.userFeed[idx].isLikedByMe = true;
-    //           draft.userFeed[idx].likes += 1;
-    //         }
-    //       })
-    //     );
-
-    //     const postsPatchResult = dispatch(
-    //       enhancedApi.util.updateQueryData("Posts", undefined, draft => {
-    //         const idx = draft.posts.findIndex(post => post.id === postId);
-    //         if (idx > -1) {
-    //           draft.posts[idx].isLikedByMe = true;
-    //           draft.posts[idx].likes += 1;
-    //         }
-    //       })
-    //     );
-
-    //     try {
-    //       await queryFulfilled;
-    //     } catch {
-    //       userFeedPatchResult.undo();
-    //       postsPatchResult.undo();
-    //     }
-    //   }
-    // },
-    // Dislike: {
-    //   invalidatesTags: (result, _, { postId }) =>
-    //     result?.dislike ? [{ type: "Post", id: postId }, "Likes"] : [],
-    //   onQueryStarted: async ({ postId }, { dispatch, queryFulfilled }) => {
-    //     const userFeedPatchResult = dispatch(
-    //       enhancedApi.util.updateQueryData("UserFeed", undefined, draft => {
-    //         const idx = draft.userFeed.findIndex(post => post.id === postId);
-    //         if (idx > -1) {
-    //           draft.userFeed[idx].isLikedByMe = false;
-    //           draft.userFeed[idx].likes -= 1;
-    //         }
-    //       })
-    //     );
-
-    //     const postsPatchResult = dispatch(
-    //       enhancedApi.util.updateQueryData("Posts", undefined, draft => {
-    //         const idx = draft.posts.findIndex(post => post.id === postId);
-    //         if (idx > -1) {
-    //           draft.posts[idx].isLikedByMe = false;
-    //           draft.posts[idx].likes -= 1;
-    //         }
-    //       })
-    //     );
-
-    //     try {
-    //       await queryFulfilled;
-    //     } catch {
-    //       userFeedPatchResult.undo();
-    //       postsPatchResult.undo();
-    //     }
-    //   }
-    // },
-    AddBookmark: {
-      invalidatesTags: (result, _, { postId }) =>
-        result?.addBookmark ? [{ type: "Post", id: postId }, "Bookmarks"] : [],
-      onQueryStarted: async ({ postId }, { dispatch, queryFulfilled }) => {
-        const userFeedPatchResult = dispatch(
-          enhancedApi.util.updateQueryData("UserFeed", undefined, draft => {
-            const idx = draft.userFeed.findIndex(post => post.id === postId);
-            if (idx > -1) {
-              draft.userFeed[idx].isBookmarkedByMe = true;
-            }
-          })
-        );
-
-        const postsPatchResult = dispatch(
-          enhancedApi.util.updateQueryData("Posts", undefined, draft => {
-            const idx = draft.posts.findIndex(post => post.id === postId);
-            if (idx > -1) {
-              draft.posts[idx].isBookmarkedByMe = true;
-            }
-          })
-        );
-
-        try {
-          await queryFulfilled;
-        } catch {
-          userFeedPatchResult.undo();
-          postsPatchResult.undo();
-        }
-      }
-    },
-    RemoveBookmark: {
-      invalidatesTags: (result, _, { postId }) =>
-        result?.removeBookmark
-          ? [{ type: "Post", id: postId }, "Bookmarks"]
-          : [],
-      onQueryStarted: async ({ postId }, { dispatch, queryFulfilled }) => {
-        const userFeedPatchResult = dispatch(
-          enhancedApi.util.updateQueryData("UserFeed", undefined, draft => {
-            const idx = draft.userFeed.findIndex(post => post.id === postId);
-            if (idx > -1) {
-              draft.userFeed[idx].isBookmarkedByMe = false;
-            }
-          })
-        );
-
-        const postsPatchResult = dispatch(
-          enhancedApi.util.updateQueryData("Posts", undefined, draft => {
-            const idx = draft.posts.findIndex(post => post.id === postId);
-            if (idx > -1) {
-              draft.posts[idx].isBookmarkedByMe = false;
-            }
-          })
-        );
-
-        try {
-          await queryFulfilled;
-        } catch {
-          userFeedPatchResult.undo();
-          postsPatchResult.undo();
         }
       }
     }
