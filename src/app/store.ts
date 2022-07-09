15e94ee51fd06e37de7864566141a8cb68051c5b
Darshan Damre <darshandamre5@gmail.com>
2022-07-09 11:47:16 +0530

    remove unused reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { alertReducer } from "../features/alert/alertSlice";
-import { api } from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
-    [api.reducerPath]: api.reducer,
     alert: alertReducer
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(api.middleware)
+  }
 });
-
-setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
